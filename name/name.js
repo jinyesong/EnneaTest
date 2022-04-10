238e7ee1fdf59da2e58e34dbe3a85e3910e545c8
Yejin Song <songyejin2000@gmail.com>
2022-04-10 20:01:05 +0900

    edit prologuePage background pc version
    help

--- name/name.js
+++ name/name.js
@@ -1,1 +1,5 @@
-// 이름, 시간 session에 저장
+// nextBtn 클릭시 prologue페이지 등장
+document.getElementById("nextBtn").addEventListener("click", function(){
+    document.getElementById("back").style.display = "none";
+    document.getElementById("prologuePage").style.display = "block";
+});
