b8ecc5684f98ce1bcbe91bfc2f3052819b864c05
Yejin Song <songyejin2000@gmail.com>
2022-04-12 02:58:20 +0900

    add chapter1

---
+++ chapter/chapter.js
@@ -0,0 +1,4 @@
+document.getElementById("nextBtn").addEventListener("click", function(){
+    document.getElementById("back").style.display = "block";
+    document.getElementsByClassName("chapterPage")[0].style.display = "none";
+});
