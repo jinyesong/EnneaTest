ff0b011184b592abf1246f5870098e18c1e5434f
Yejin Song <songyejin2000@gmail.com>
2022-07-09 21:38:58 +0900

    fixed indexPage

--- public/index.js
+++ public/index.js
@@ -5,10 +5,25 @@ document.getElementById("openBtn").addEventListener("click", function(){
     document.getElementById("prologue").style.display = "flex";
 });
 
-setInterval(function(){
-    if($("#video").prop("ended")){
-      //영상종료 후 진행할 함수 입력부분
-      document.getElementById("nextBtn").style.display = "block";
-    }
-  },200);
-//0.2초마다 비디오의 속성 ended의 값이 무엇인지 검사한다
+if(isMobile()){
+  //시작페이지
+  let mainDiv = document.getElementById("page_container");
+  mainDiv.style.width = "100%";
+
+  let mainGif = document.getElementById("mainGif");
+  mainGif.style.width = "100%";
+
+  //프롤로그
+  let proGif =  document.getElementById("prologueGif");
+  proGif.src = "/image/prologueVideo_mb.gif";
+  proGif.style.width = "100%";
+
+  let btn = document.getElementById("nextBtn");
+  btn.style.top = "90%";
+  btn.style.left = "80%";
+}
+
+function isMobile() {
+  console.log(navigator.userAgent);
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
