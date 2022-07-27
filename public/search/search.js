8164aa0aa72c2230f6a33a8e68e262ffb7a29409
Yejin Song <songyejin2000@gmail.com>
2022-07-28 01:35:02 +0900

    add detail popup but unfinished

--- public/search/search.js
+++ public/search/search.js
@@ -13,7 +13,7 @@ const db = firebase.firestore();
 let getArr = new Array(); //처음에 목록을 받아와서 이후에 검색할 때 또 받아오지 않도록 저장해두는 배열
 db.collection("User").orderBy('time', 'desc').get().then((snapshot)=>{
     snapshot.forEach((doc)=>{
-        let arr = new Array(11);
+        let arr = new Array(12);
         let date = new Date(doc.data().time*1000);
         let year = Number(date.getFullYear()-1969); //왜그런진 모르겠는데 년도가 3991로 나옴
         let month = ("0" + Number(date.getMonth()+1)).slice(-2);
@@ -31,12 +31,17 @@ db.collection("User").orderBy('time', 'desc').get().then((snapshot)=>{
         arr[8] = doc.data().seven;
         arr[9] = doc.data().eight;
         arr[10] = doc.data().nine;
-
+        arr[11] = doc.id; //getArr 마지막 요소로 문서 id 추가
         getArr.push(arr);
+
         let TR = document.createElement("tr");
         for(let i=0; i<11; i++){
             let TD = document.createElement("td");
             TD.innerHTML = arr[i];
+            if(i == 1){
+                TD.setAttribute("id", doc.id);
+                TD.setAttribute("class", "title");
+            }
             TR.appendChild(TD);
         }
         document.getElementById("tableBody").appendChild(TR);
@@ -56,6 +61,10 @@ document.getElementById("searchBtn").addEventListener("click", function(){
                 for(let j=0; j<11; j++){
                     let TD = document.createElement("td");
                     TD.innerHTML = getArr[i][j];
+                    if(j == 1){
+                        TD.setAttribute("id", getArr[i][11]);
+                        TD.setAttribute("class", "title");
+                    }
                     TR.appendChild(TD);
                 }
                 document.getElementById("tableBody").appendChild(TR);
@@ -71,6 +80,10 @@ document.getElementById("searchBtn").addEventListener("click", function(){
                 for(let j=0; j<11; j++){
                     let TD = document.createElement("td");
                     TD.innerHTML = getArr[i][j];
+                    if(j == 1){
+                        TD.setAttribute("id", getArr[i][11]);
+                        TD.setAttribute("class", "title");
+                    }
                     TR.appendChild(TD);
                 }
                 document.getElementById("tableBody").appendChild(TR);
@@ -85,6 +98,10 @@ document.getElementById("searchBtn").addEventListener("click", function(){
                 for(let j=0; j<11; j++){
                     let TD = document.createElement("td");
                     TD.innerHTML = getArr[i][j];
+                    if(j == 1){
+                        TD.setAttribute("id", getArr[i][11]);
+                        TD.setAttribute("class", "title");
+                    }
                     TR.appendChild(TD);
                 }
                 document.getElementById("tableBody").appendChild(TR);
@@ -94,4 +111,16 @@ document.getElementById("searchBtn").addEventListener("click", function(){
     else{
         alert("날짜 혹은 이름을 입력해주세요.")
     }
+});
+
+//팝업 상세보기 기능
+document.getElementById("tableBody").addEventListener("click", function(e){
+    console.log(e.target.className);
+    if(e.target.className == "title"){
+        document.getElementById("popupContainer").style.display = "block";
+    }
+});
+
+document.getElementById("popup_cancel").addEventListener("click", function(){
+    document.getElementById("popupContainer").style.display = "none";
 });
