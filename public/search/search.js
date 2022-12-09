c29042682f42e7f82d13595c3e6300be2374e2b0
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2022-12-09 15:05:46 +0900

    renew data load

--- public/search/search.js
+++ public/search/search.js
@@ -144,69 +144,147 @@ document.getElementById("logout").addEventListener("click", function(){ //로그
 
 makeQuestionbar();
 let getArr = new Array(); //처음에 목록을 받아와서 이후에 검색할 때 또 받아오지 않도록 저장해두는 배열
-db.collection("User").orderBy('time', 'desc').get().then((snapshot)=>{
-    snapshot.forEach((doc)=>{
-        let arr = new Array(12);
-        let date = new Date(doc.data().time*1000);
-        let year = Number(date.getFullYear()-1969); //왜그런진 모르겠는데 년도가 3991로 나옴
-        let month = ("0" + Number(date.getMonth()+1)).slice(-2);
-        let day = ("0" + date.getDate()).slice(-2);
-        let hour = ("0" + date.getHours()).slice(-2);
-        let minute = ("0" + date.getMinutes()).slice(-2);
-        arr[0] = year + "-" + month + "-" + day + " " + hour + ":" + minute;
-        arr[1] = doc.data().name;
-        arr[2] = doc.data().one;
-        arr[3] = doc.data().two;
-        arr[4] = doc.data().three;
-        arr[5] = doc.data().four;
-        arr[6] = doc.data().five;
-        arr[7] = doc.data().six;
-        arr[8] = doc.data().seven;
-        arr[9] = doc.data().eight;
-        arr[10] = doc.data().nine;
-        arr[11] = doc.id; //getArr 마지막 요소로 문서 id 추가
-        arr[12] = doc.data().allScore; // 진짜 마지막 요소로 전체 score 데이터 추가
-        // console.log(doc.data().allScore);
-        getArr.push(arr);
+// db.collection("User").orderBy('time', 'desc').get().then((snapshot)=>{
+//     snapshot.forEach((doc)=>{
+//         let arr = new Array(12);
+//         let date = new Date(doc.data().time*1000);
+//         let year = Number(date.getFullYear()-1969); //왜그런진 모르겠는데 년도가 3991로 나옴
+//         let month = ("0" + Number(date.getMonth()+1)).slice(-2);
+//         let day = ("0" + date.getDate()).slice(-2);
+//         let hour = ("0" + date.getHours()).slice(-2);
+//         let minute = ("0" + date.getMinutes()).slice(-2);
+//         arr[0] = year + "-" + month + "-" + day + " " + hour + ":" + minute;
+//         arr[1] = doc.data().name;
+//         arr[2] = doc.data().one;
+//         arr[3] = doc.data().two;
+//         arr[4] = doc.data().three;
+//         arr[5] = doc.data().four;
+//         arr[6] = doc.data().five;
+//         arr[7] = doc.data().six;
+//         arr[8] = doc.data().seven;
+//         arr[9] = doc.data().eight;
+//         arr[10] = doc.data().nine;
+//         arr[11] = doc.id; //getArr 마지막 요소로 문서 id 추가
+//         arr[12] = doc.data().allScore; // 진짜 마지막 요소로 전체 score 데이터 추가
+//         // console.log(doc.data().allScore);
+//         getArr.push(arr);
 
-        let TR = document.createElement("tr");
-        for(let i=0; i<11; i++){
-            let TD = document.createElement("td");
-            TD.innerHTML = arr[i];
-            if(i == 1){
-                TD.setAttribute("id", doc.id);
-                TD.setAttribute("class", "title");
-                TD.setAttribute("data-allScore", doc.data().allScore);
-            }
-            TR.appendChild(TD);
-        }
-        document.getElementById("tableBody").appendChild(TR);
+//         let TR = document.createElement("tr");
+//         for(let i=0; i<11; i++){
+//             let TD = document.createElement("td");
+//             TD.innerHTML = arr[i];
+//             if(i == 1){
+//                 TD.setAttribute("id", doc.id);
+//                 TD.setAttribute("class", "title");
+//                 TD.setAttribute("data-allScore", doc.data().allScore);
+//             }
+//             TR.appendChild(TD);
+//         }
+//         document.getElementById("tableBody").appendChild(TR);
+//     })
+// })
+
+start_data = "";
+console.log("데이터 읽기 시작");
+db.collection("User").orderBy("time", "desc").orderBy("one").endBefore(5).limit(10).get().then(function(documentSnapshots) {
+    documentSnapshots.forEach(function(doc) {
+        makeTable(doc);
+    });
+    start_data = documentSnapshots.docs[documentSnapshots.docs.length-1];
+});
+    
+
+document.getElementById("moreData").addEventListener("click", function(){
+    db.collection("User").orderBy("time", "desc").orderBy("one").endBefore(5).startAfter(start_data).limit(5).get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            makeTable(doc);
+        });
+        start_data = querySnapshot.docs[querySnapshot.docs.length-1];
     })
-})
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+});
+
+function makeTable (doc) {
+    let arr = new Array(12);
+    let date = new Date(doc.data().time*1000);
+    let year = Number(date.getFullYear()-1969); //왜그런진 모르겠는데 년도가 3991로 나옴
+    let month = ("0" + Number(date.getMonth()+1)).slice(-2);
+    let day = ("0" + date.getDate()).slice(-2);
+    let hour = ("0" + date.getHours()).slice(-2);
+    let minute = ("0" + date.getMinutes()).slice(-2);
+    arr[0] = year + "-" + month + "-" + day + " " + hour + ":" + minute;
+    arr[1] = doc.data().name;
+    arr[2] = doc.data().one;
+    arr[3] = doc.data().two;
+    arr[4] = doc.data().three;
+    arr[5] = doc.data().four;
+    arr[6] = doc.data().five;
+    arr[7] = doc.data().six;
+    arr[8] = doc.data().seven;
+    arr[9] = doc.data().eight;
+    arr[10] = doc.data().nine;
+    arr[11] = doc.id; //getArr 마지막 요소로 문서 id 추가
+    arr[12] = doc.data().allScore; // 진짜 마지막 요소로 전체 score 데이터 추가
+    // console.log(doc.data().allScore);
+    getArr.push(arr);
+
+    let TR = document.createElement("tr");
+    for(let i=0; i<11; i++){
+        let TD = document.createElement("td");
+        TD.innerHTML = arr[i];
+        if(i == 1){
+            TD.setAttribute("id", doc.id);
+            TD.setAttribute("class", "title");
+            TD.setAttribute("data-allScore", doc.data().allScore);
+        }
+        TR.appendChild(TD);
+    }
+    document.getElementById("tableBody").appendChild(TR);
+}
 
 //검색 기능
 document.getElementById("searchBtn").addEventListener("click", function(){
+    console.log(getArr);
     let searchDate = document.getElementById("dateSearchBox").value;
     let searchName = document.getElementById("nameSearchBox").value;
     //let tbodyRow = document.getElementById("tableBody").getElementsByTagName("tr");
     if(searchDate === "" && searchName !== ""){ //이름으로만 검색
         $("#tableBody").empty(); //테이블 비우기
-        for(let i=0; i<getArr.length; i++){
-            if(getArr[i][1].includes(searchName)){ //왜 오류가날까->null문자열때문이었음
-                let TR = document.createElement("tr");
-                for(let j=0; j<11; j++){
-                    let TD = document.createElement("td");
-                    TD.innerHTML = getArr[i][j];
-                    if(j == 1){
-                        TD.setAttribute("id", getArr[i][11]);
-                        TD.setAttribute("class", "title");
-                        TD.setAttribute("data-allScore", getArr[i][12]);
-                    }
-                    TR.appendChild(TD);
-                }
-                document.getElementById("tableBody").appendChild(TR);
-            }
+        if(getArr[i][1] != null) {
+            db.collection("User").orderBy("name", "desc").where("name", ">=", searchName).limit(3).get().then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    makeTable(doc);
+                });
+                start_data = querySnapshot.docs[querySnapshot.docs.length-1];
+            })
+            .catch(function(error) {
+                console.log("Error getting documents: ", error);
+            });
         }
+        
+
+
+        // for(let i=0; i<getArr.length; i++){
+        //     if(getArr[i][1] == null) {
+        //         continue;
+        //     }
+        //     if(getArr[i][1].includes(searchName)){ //왜 오류가날까->null문자열때문이었음
+        //         let TR = document.createElement("tr");
+        //         for(let j=0; j<11; j++){
+        //             let TD = document.createElement("td");
+        //             TD.innerHTML = getArr[i][j];
+        //             if(j == 1){
+        //                 TD.setAttribute("id", getArr[i][11]);
+        //                 TD.setAttribute("class", "title");
+        //                 TD.setAttribute("data-allScore", getArr[i][12]);
+        //             }
+        //             TR.appendChild(TD);
+        //         }
+        //         document.getElementById("tableBody").appendChild(TR);
+        //     }
+        // }
     }
     else if(searchName === "" && searchDate !== ""){ //날짜로만 검색
         $("#tableBody").empty(); //테이블 비우기
